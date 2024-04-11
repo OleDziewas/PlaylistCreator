895445c2a80e7d9a94aa283ccfd0a79ce2118059
OleDziewas <o.dziewas@t-online.de>
2024-04-11 18:21:24 +0200

    Finished input layout

--- src/routes/PlaylistInput.svelte
+++ src/routes/PlaylistInput.svelte
@@ -43,10 +43,22 @@
                 </div>
                 <br>
             </div>
-            <div class="md:w-1/2 w-full bg-blue-400">
-                <h3>Genre</h3>
+            <div class="grid grid-cols-4 gap-4 md:w-1/2 w-full">
+                <h3 class="block text-gray-700 text-xl font-bold mb-2 col-span-4">Genre:</h3>
                 {#each genres as genre}
-                    <label>{genre}<input type="checkbox" name={genre}></label><br>
+                    <div class="col-span-1 m-2 flex flex-row justify-start items-center">
+                        <input class="before:content[''] peer relative h-8 w-8 cursor-pointer appearance-none rounded-full border border-gray-900/20 bg-gray-900/10 transition-all before:absolute before:top-2/4 before:left-2/4 before:block before:h-12 before:w-12 before:-translate-y-2/4 before:-translate-x-2/4 before:rounded-full before:bg-blue-gray-500 before:opacity-0 before:transition-opacity checked:border-gray-900 checked:bg-green-500 checked:before:bg-gray-900 hover:scale-105 hover:before:opacity-0" id={genre} type="checkbox" name={genre}>
+                        <span
+                            class="absolute text-white transition-opacity opacity-0 pointer-events-none top-2/4 left-2/4 -translate-y-2/4 -translate-x-2/4 peer-checked:opacity-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-3.5 w-3.5" viewBox="0 0 20 20" fill="currentColor"
+                                stroke="currentColor" stroke-width="1">
+                                <path fill-rule="evenodd"
+                                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                                clip-rule="evenodd"></path>
+                            </svg>
+                        </span>
+                        <label class="pl-5" for={genre}>{genre}</label>
+                    </div>
                 {/each}
             </div>
         </div>
@@ -57,7 +69,7 @@
         {:else}
             <div class="container md:w-1/3 w-full flex flex-row items-center justify-center">
                 <form method="POST" action="?/login">
-                    <button class="m-6 text-xl bg-green-600 hover:bg-gray-100 text-white font-bold py-2 px-4  rounded-3xl shadow" type="submit">Create Playlist</button>
+                    <button class="m-6 text-xl bg-green-600 hover:bg-green-300 text-white font-bold py-2 px-4  rounded-3xl shadow" type="submit">Create Playlist</button>
                 </form>
             </div>
         {/if}
